5e8a023c35b182e3e3e17aa10cc81e4c3dd7e8a1
Aaron <aakiswa@gmail.com>
2024-04-18 16:28:51 +0800

    Finish exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+      if(num < 0){
+            return 'ERROR'
+      }else{
+            return string.repeat(num);
+      } 
 };
+
+repeatString("hey", 3);
+repeatString("hey", 10);
+repeatString("hey", 1);
+repeatString("hey", 0);
+repeatString("hey", -1);
+repeatString("", 10);
 
 // Do not edit below this line
 module.exports = repeatString;
