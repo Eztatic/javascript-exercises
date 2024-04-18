e3d5588a9c9d45b806a826cadec5b12659af4a2b
Aaron <aakiswa@gmail.com>
2024-04-18 19:53:25 +0800

    Finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+      let reversedString = "";
+      let g = string.length-1;
+      string.split();
+      for(let i = 1; i <= string.length; i++) {
+            reversedString = reversedString.concat(string[g]);
+            g--;
+      }
+      return reversedString;
 };
+
+reverseString("hello");
+reverseString("hello there");
+reverseString("123! abc!");
+reverseString("");
 
 // Do not edit below this line
 module.exports = reverseString;
