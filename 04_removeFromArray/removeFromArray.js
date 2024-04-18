c7331478ef01713a66cdd7e998eecd6746659b8a
Aaron <aakiswa@gmail.com>
2024-04-18 22:59:00 +0800

    Finished Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+      let newArray = [];
+      array.forEach((element) => {
+            if(!args.includes(element)) {
+                  //array.splice(array.indexOf(element), 1);
+                  newArray.push(element);
+            }     
+      });
+      return newArray;
 };
+
+removeFromArray([1, 2, 3, 4], 3);
+removeFromArray([1, 2, 3, 4], 3, 2);
+removeFromArray([1, 2, 2, 3], 2);
+removeFromArray([1, 2, 3, 4], 7, "tacos");
+removeFromArray([1, 2, 3, 4], 7, 2);
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4);
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
+removeFromArray([1, 2, 3], "1", 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
