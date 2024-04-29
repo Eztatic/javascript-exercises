a7db5b4b2a9ef4baea9afa1f76c75f2d985f6b20
Aaron <aakiswa@gmail.com>
2024-04-29 22:31:30 +0800

    Done exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+      let reversed = string.replace(/[^a-zA-Z0-9]/g, '')
+                           .toLowerCase()
+                           .split('')
+                           .reverse()
+                           .join('');
+      let noSpecChar = string.replace(/[^a-zA-Z0-9]/g, '')
+                             .toLowerCase()
+                             .split('')
+                             .join('');
+      for(let i = 0; i <= string.length; i++){
+            if(reversed[i] !== noSpecChar[i]){
+                  return false;
+            }
+      }
+      return true;
 };
 
 // Do not edit below this line
