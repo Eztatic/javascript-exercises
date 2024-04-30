b00b352344d17e076a02ee8e25b380638c648f4f
Aaron <aakiswa@gmail.com>
2024-04-30 21:38:41 +0800

    Done exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+      if(num == 0){
+            return 0;
+      }else if(num < 0){
+            return 'OOPS';
+      }
+      let fibArray = [1, 1];
+      for(let i = 0; i < num-2; i++) {
+            fibArray.push(fibArray[i] + fibArray[i+1]);
+      }
+      return fibArray[num-1];
 };
 
 // Do not edit below this line
