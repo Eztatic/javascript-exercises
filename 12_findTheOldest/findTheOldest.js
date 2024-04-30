3a9dc8af0a5b6974f7fa5c3f8f459fc46740f2d0
Aaron <aakiswa@gmail.com>
2024-04-30 22:58:36 +0800

    Done exercse 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+      let currentYear = new Date().getFullYear();
+      return arr.sort((person1, person2) => {
+            let deathYr1 = person1.yearOfDeath;
+            let deathYr2 = person2.yearOfDeath;
+            if(person1.yearOfDeath === undefined){
+                  deathYr1 = currentYear;
+            }else if(person2.yearOfDeath === undefined){
+                  deathYr2 = currentYear;
+            }
+            let age1 = deathYr1 - person1.yearOfBirth;
+            let age2 = deathYr2 - person2.yearOfBirth;
+            return age1 - age2;
 
+      }).findLast(() => true);
 };
 
 // Do not edit below this line
